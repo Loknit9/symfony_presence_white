69ade4aebd4c4e6b0a7d69f765e9230cafe639f5
Laurence Delforge <laurencedelforge@hotmail.com>
2023-09-21 16:13:06 +0200

    auth

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        for($i=0; $i <20; $i++){
+            $user = new User();
+            $user->setEmail ("user".$i."@gmail.com");
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                'paswword'.$i
+            ));
+            $user->setNom ("nom".$i);
+            $user->setPrenom ("prenom".$i);
+            $user->setDateNaissance ();
+            $user->setContact1 ("contact1".$i."@gmail.com");
+            $user->setContact2 ("contact2".$i."@gmail.com");
+            
+            $manager->persist($user);
+
+        }
+
+        $manager->flush();
+    }
+}
