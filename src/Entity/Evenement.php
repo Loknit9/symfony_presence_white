d6720f918d469ba7385bcdba5451f7127b84ef35
Laurence Delforge <laurencedelforge@hotmail.com>
2023-10-09 15:16:08 +0200

    evenement propriete

--- src/Entity/Evenement.php
+++ src/Entity/Evenement.php
@@ -16,9 +16,6 @@ class Evenement
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
-    private ?\DateTimeInterface $dateEvenement = null;
-
     #[ORM\Column(length: 255)]
     private ?string $typeEvenement = null;
 
@@ -28,7 +25,20 @@ class Evenement
     #[ORM\ManyToMany(targetEntity: Equipe::class, mappedBy: 'evenement')]
     private Collection $equipes;
 
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $start = null;
 
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $end = null;
+
+    #[ORM\Column(length: 7)]
+    private ?string $background_color = null;
+
+    #[ORM\Column(length: 7)]
+    private ?string $border_color = null;
+
+    #[ORM\Column(length: 7)]
+    private ?string $text_color = null;
 
     public function hydrate(array $vals)
     {
@@ -50,18 +60,6 @@ class Evenement
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getDateEvenement(): ?\DateTimeInterface
-    {
-        return $this->dateEvenement;
-    }
-
-    public function setDateEvenement(\DateTimeInterface $dateEvenement): static
-    {
-        $this->dateEvenement = $dateEvenement;
-
-        return $this;
     }
 
     public function getTypeEvenement(): ?string
@@ -129,6 +127,66 @@ class Evenement
         if ($this->equipes->removeElement($equipe)) {
             $equipe->removeEvenement($this);
         }
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeInterface $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTimeInterface
+    {
+        return $this->end;
+    }
+
+    public function setEnd(\DateTimeInterface $end): static
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+
+    public function getBackgroundColor(): ?string
+    {
+        return $this->background_color;
+    }
+
+    public function setBackgroundColor(string $background_color): static
+    {
+        $this->background_color = $background_color;
+
+        return $this;
+    }
+
+    public function getBorderColor(): ?string
+    {
+        return $this->border_color;
+    }
+
+    public function setBorderColor(string $border_color): static
+    {
+        $this->border_color = $border_color;
+
+        return $this;
+    }
+
+    public function getTextColor(): ?string
+    {
+        return $this->text_color;
+    }
+
+    public function setTextColor(string $text_color): static
+    {
+        $this->text_color = $text_color;
 
         return $this;
     }
