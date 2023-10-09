39e4382d898b429c4d5b4e17b45035f0968b4911
Laurence Delforge <laurencedelforge@hotmail.com>
2023-10-09 09:23:48 +0200

    nom equipe

--- migrations/Version20231009072217.php
+++ migrations/Version20231009072217.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231008133733 extends AbstractMigration
+final class Version20231009072217 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/EquipeFixtures.php
+++ src/DataFixtures/EquipeFixtures.php
@@ -19,15 +19,16 @@ class EquipeFixtures extends Fixture implements DependentFixtureInterface
 
         $faker = Factory::create('fr_FR');
 
-        $categorieAge = array(5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 19);
-        $nom = array(1, 2, 3, 4, 5, 6);
+        $categorieAge= array(5,6,7,8,9,10,11,12,14,16,19);
+        $categorie = array('U5', 'U6', 'U7', 'U8', 'U9', 'U10', 'U11', 'U12', 'U14', 'U16', 'U19');
+        $nom = array(1,2,3,4);
 
         for ($i = 0; $i < 4; $i++) {
             $equipe = new Equipe([
-                'nom' => $nom[mt_rand(0, count($nom) - 1)],
+                'nom' => ($categorie[mt_rand(0, count($categorieAge) - 1)]).($faker->randomElement(['G', 'B'])).($nom[mt_rand(0, count($nom) - 1)]),
                 'categorieAge' => $categorieAge[mt_rand(0, count($categorieAge) - 1)],
 
-                'categorieGenre' => $faker->randomElement(['G', 'B']),
+                'categorieGenre' => $faker->randomElement(['fille', 'garçon']),
             ]);
 
             $manager->persist($equipe);
