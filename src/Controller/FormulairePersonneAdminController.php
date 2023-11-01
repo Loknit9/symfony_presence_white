11894be74ff5cfc1d708992f2dd5e5c4713a0450
Loknit9 <laurencedelforge@hotmail.com>
2023-11-01 12:27:15 +0100

    add personne

--- src/Controller/FormulairePersonneAdminController.php
+++ src/Controller/FormulairePersonneAdminController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
 use App\Form\PersonneType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormulairePersonneAdminController extends AbstractController
 {
+    //creer le formulaire
     #[Route('/form/personne/admin', name: 'form_personne')]
+
     public function index(): Response
     {
         $formPersonne = $this->createForm(PersonneType::class);
@@ -18,8 +23,39 @@ class FormulairePersonneAdminController extends AbstractController
 
         return $this->render('form_personne_admin/form.html.twig', $vars );
     }
+
+// ajouter un joueur ou un coach, afficher et traiter le formulaire//
+
+    #[Route('/personne/add', name: 'addForm_personne')]
+    public function personneAdd(Request $req, ManagerRegistry $doctrine)
+    {
+       // créer une entité vide
+        $personne = new Personne();
+        
+       // créer un objet formulaire et associer l'entité à cet obet formulaire
+
+        $formPersonne = $this->createForm(PersonneType::class, $personne);
+
+       // traiter la requête. Si on a fait un submit, l'entité $personne sera remplie
+       // avec les données du form
+        $formPersonne->handleRequest($req);
+
+       // on vient d'un submit
+        if ($formPersonne->isSubmitted()) {
+           // formulaire soumis, stocker dans la BD
+
+        $em = $doctrine->getManager();
+        $em->persist($personne);
+        $em->flush();
+
+           // pour éviter une nouvelle insertion si on recharge la page, on va charger une autre action
+           // redirectToRoute reçoit le nom d'une route
+        return $this->redirectToRoute("addForm_personne");
+
+        }
+    }
+
+// obtenir un formulaire pré-rempli pour updater les infos d'un joueur ou un coach //
+
+
 }
-
-
-// obtenir un formulaire pré-rempli pour updater un joueur ou un coach //
-
