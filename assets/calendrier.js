b801e9a53d6e1d22a09e5f5002516cc04feb1135
Loknit9 <laurencedelforge@hotmail.com>
2023-11-09 21:22:02 +0100

    route calendrier

--- assets/calendrier.js
+++ assets/calendrier.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
         dateClick: function (info) {
 
             console.log(div_calendrier.dataset.equipe);
-            window.location.href = "/evenement/" + info.dateStr + "/" + div_calendrier.dataset.equipe;
+            window.location.href = divCalendar.dataset.route;
         },
 
         //afficher la page URL de l'evt lorsqu'on clique sur un evenement qui se trouve dans le calendrier et les présences pour ce jour
